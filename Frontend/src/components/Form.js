fe498e66c23a1345900f5431ac2d4cf29de91122
kamal-web <kamalakar.kathi1650@gmail.com>
2019-12-18 13:46:42 +0530

    form

---
+++ Frontend/src/components/Form.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Form = (props) => {
+    const {myKey:keys,empData:data} = props;
+  return (
+    <div className="from-block">
+      <form className="form" onSubmit={event => props.submitHandler(event)}>
+        {keys.map(name => {
+          return (
+            <div>
+              {name}:
+              <input
+                type="text"
+                name={name}
+                defaultValue={data[name]}
+                onChange={e => {
+                  props.changeHandler(e);
+                }}
+              />
+            </div>
+          );
+        })}
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  );
+}
+
+export default Form;
